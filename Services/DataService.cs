0bfc24958d11b3f9ad3657505f630796b7e9d82c
Ged Moretta <gedmoretta@gmail.com>
2022-03-13 18:24:58 +1000

    ingredients as list

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -4,29 +4,34 @@ namespace toomanycookbooks.Services
     {
         public Recipe() { }
 
-        public Recipe(string name, string book, string author, string ingredients)
+        public Recipe(string name, string book, string author, string[] ingredients)
         {
             Name = name;
             Book = book;
             Author = author;
-            Ingredients = ingredients;
+            Ingredients.AddRange(ingredients.Select(i => new Ingredient{ Name = i }));
         }
 
         public string? Name { get; set; }
         public string? Book { get; set; }
         public string? Author { get; set; }
 
-        public string? Ingredients { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new();
+    }
+
+    public class Ingredient 
+    {
+        public string Name { get; set; } = "";
     }
 
     public class DataService
     {
         private List<Recipe> _recipes = new List<Recipe>
             {
-                new Recipe("Recipe1", "Book1", "Author1", "Ingredient1,Ingredient2"),
-                new Recipe("Recipe2", "Book2", "Author2", "Ingredient2,Ingredient3"),
-                new Recipe("Recipe3", "Book1", "Author1", "Ingredient3,Ingredient4"),
-                new Recipe("Recipe4", "Book3", "Author3", "Ingredient4,Ingredient1")
+                new Recipe("Recipe1", "Book1", "Author1", new[] { "Ingredient1,Ingredient2" }),
+                new Recipe("Recipe2", "Book2", "Author2", new[] { "Ingredient2,Ingredient3" }),
+                new Recipe("Recipe3", "Book1", "Author1", new[] { "Ingredient3,Ingredient4" }),
+                new Recipe("Recipe4", "Book3", "Author3", new[] { "Ingredient4,Ingredient1" })
             };
 
         public IEnumerable<Recipe> GetRecipes()
