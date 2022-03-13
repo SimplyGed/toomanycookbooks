562cd4f7bf1cb0786f5d5fefb47439c7e375f433
Ged Moretta <gedmoretta@gmail.com>
2022-03-13 17:08:00 +1000

    add ingredient page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using Blazorise;
-using Blazorise.Bootstrap;
-using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using toomanycookbooks;
@@ -12,12 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<DataService>();
-
-builder.Services
-    .AddBlazorise(options => {
-        options.Immediate = true;
-    })
-    .AddBootstrapProviders()
-    .AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();
--- Services/DataService.cs
+++ Services/DataService.cs
@@ -1,18 +1,44 @@
 namespace toomanycookbooks.Services
 {
-    public record Recipe(string Name, string Book, string Author, string Ingredients);
+    public class Recipe
+    {
+        public Recipe() { }
+
+        public Recipe(string name, string book, string author, string ingredients)
+        {
+            Name = name;
+            Book = book;
+            Author = author;
+            Ingredients = ingredients;
+        }
+
+        public string? Name { get; set; }
+        public string? Book { get; set; }
+        public string? Author { get; set; }
+
+        public string? Ingredients { get; set; }
+    }
 
     public class DataService
     {
-        public IEnumerable<Recipe> GetRecipes()
-        {
-            return new List<Recipe>
+        private List<Recipe> _recipes = new List<Recipe>
             {
                 new Recipe("Recipe1", "Book1", "Author1", "Ingredient1,Ingredient2"),
                 new Recipe("Recipe2", "Book2", "Author2", "Ingredient2,Ingredient3"),
                 new Recipe("Recipe3", "Book1", "Author1", "Ingredient3,Ingredient4"),
                 new Recipe("Recipe4", "Book3", "Author3", "Ingredient4,Ingredient1")
             };
+
+        public IEnumerable<Recipe> GetRecipes()
+        {
+            return _recipes;
+        }
+
+        public bool Save(Recipe recipe)
+        {
+            _recipes.Add(recipe);
+
+            return true;
         }
     }
 }
